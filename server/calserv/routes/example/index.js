b49f0f5dbbffecb37affd6d211810685fbe55cb7
Liam Tirpitz <tirpitz@informatik.rwth-aachen.de>
2024-09-26 10:57:29 +0200

    Start ical parsing

--- server/calserv/routes/example/index.js
+++ server/calserv/routes/example/index.js
@@ -1,7 +1,28 @@
 'use strict'
 
+const ical = require("node-ical");
+
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return 'this is an example'
+    var config = require('../../config/calendars.json');
+    let endpoint = config.calendars[0].endpoint
+    console.log(endpoint)
+    const data = await getCalendar(endpoint)
+    for (let k in data) {
+      const event = data[k];
+      console.log(event.type)
+    }
+    return data
   })
 }
+
+function parseEvent(ev) {
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  return `${ev.summary} is in ${ev.location} on the ${ev.start.getDate()} of ${months[ev.start.getMonth()]} at ${ev.start.toLocaleTimeString('en-GB')}\r\n`;
+}
+
+async function getCalendar(endpoint){
+  const ical = require('node-ical');
+  return ical.async.fromURL(endpoint);
+}
+
