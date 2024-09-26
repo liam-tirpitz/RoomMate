96f0e9896b3afc861835853af2b992210e4d5d08
Liam Tirpitz <tirpitz@informatik.rwth-aachen.de>
2024-09-26 18:01:08 +0200

    Some more ical parsing

--- server/calserv/routes/example/index.js
+++ server/calserv/routes/example/index.js
@@ -1,6 +1,20 @@
 'use strict'
 
 const ical = require("node-ical");
+const moment = require('moment-timezone');
+
+function SimpleEvent(start, end, summary) {
+  this.start = start;
+  this.end = end;
+  this.summary = summary;
+}
+
+function getSimpleEvent(complexEvent) {
+  return new SimpleEvent(
+      complexEvent.start.toLocaleTimeString('de-DE'),
+      complexEvent.end.toLocaleTimeString('de-DE'),
+      complexEvent.summary);
+}
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
@@ -8,12 +22,58 @@ module.exports = async function (fastify, opts) {
     let endpoint = config.calendars[0].endpoint
     console.log(endpoint)
     const data = await getCalendar(endpoint)
+    var currentTime = new Date(Date.now());
+    let endTime = new Date(Date.now() + 24 * (60 * 60 * 1000) );
+
     for (let k in data) {
       const event = data[k];
-      console.log(event.type)
+
+      if (event.type === "VEVENT") {
+
+        if (event.rrule) {
+          parseRecurrences(event, currentTime, endTime)
+        }
+        if (event.start.getTime() <= currentTime.getTime() && currentTime.getTime() <= event.end.getTime()) {
+          console.log("Occupied!");
+          console.log(getSimpleEvent(event));
+        } else if (endTime >= event.start.getTime() && event.start.getTime() >= currentTime.getTime()) {
+          console.log("Event soon!");
+          console.log(getSimpleEvent(event))
+        }
+      }
     }
     return data
   })
+}
+
+function parseRecurrences(event, start_time, end_time) {
+  const dates = event.rrule.between(start_time, end_time)
+  console.log(dates)
+  if (dates.length === 0) return;
+
+  console.log('Summary:', event.summary);
+  console.log('Original start:', event.start);
+  console.log('RRule start:', `${event.rrule.origOptions.dtstart} [${event.rrule.origOptions.tzid}]`)
+
+  dates.forEach(date => {
+    let newDate
+    if (event.rrule.origOptions.tzid) {
+      // tzid present (calculate offset from recurrence start)
+      const dateTimezone = moment.tz.zone('UTC')
+      const localTimezone = moment.tz.guess()
+      const tz = event.rrule.origOptions.tzid === localTimezone ? event.rrule.origOptions.tzid : localTimezone
+      const timezone = moment.tz.zone(tz)
+      const offset = timezone.utcOffset(date) - dateTimezone.utcOffset(date)
+      newDate = moment(date).add(offset, 'minutes').toDate()
+    } else {
+      // tzid not present (calculate offset from original start)
+      newDate = new Date(date.setHours(date.getHours() - ((event.start.getTimezoneOffset() - date.getTimezoneOffset()) / 60)))
+    }
+    const start = moment(newDate)
+    const simple_event = new SimpleEvent(newDate.toLocaleTimeString('de-DE'), newDate.toLocaleTimeString('de-DE'), event.summary); // TODO EndTime?
+    console.log('SimpleEvent:', simple_event)
+  })
+
 }
 
 function parseEvent(ev) {
