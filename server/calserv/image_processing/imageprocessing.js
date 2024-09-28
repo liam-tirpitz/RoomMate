6f4d7dc2615399f1ca92ccc26639c232c7a1bd0c
Liam Tirpitz <tirpitz@informatik.rwth-aachen.de>
2024-09-28 18:06:18 +0200

    Generate Image Header and CurrentEvent Details. Towards #4

--- server/calserv/image_processing/imageprocessing.js
+++ server/calserv/image_processing/imageprocessing.js
@@ -2,28 +2,63 @@ const {registerFont, createCanvas, loadImage } = require('canvas')
 const fs = require('fs')
 
 
+async function drawCurrentEventTime(ctx, timestring, black_font = false) {
+    ctx.fillStyle = "rgba(255, 255, 255, 1)";
+    if (black_font) {
+        ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    }
+    ctx.font = '18pt "HNB"'
+    ctx.fillText(timestring, 32, 242)
+}
 
-
-async function drawOccupied(ctx, summary, organizer, timestring) {
+async function drawCurrentEventBlackBox(ctx) {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 106, 480, 169);
+}
+
+async function drawCurrentEventWhiteBox(ctx) {
+    ctx.lineWidth = 2;
+    ctx.beginPath()
+    ctx.strokeStyle = 'black';
+    ctx.strokeRect(-2, 106, 482, 169);
+    ctx.closePath()
+}
+
+async function drawCurrentEventInfo(ctx, info, black_font = false) {
+    ctx.font = '42pt "HNB"'
+    ctx.fillStyle = "rgba(255, 255, 255, 1)";
+    if (black_font) {
+        ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    }
+    ctx.fillText(info, 32, 202)
+}
+
+async function drawOccupied(ctx, summary, organizer, timestring) {
+    await drawCurrentEventBlackBox(ctx)
     ctx.font = '18pt "HNB"'
     ctx.fillStyle = "rgba(255, 255, 255, 1";
     ctx.fillText(summary, 32, 160)
     ctx.font = '18pt "HNL"'
     ctx.fillText(organizer, 32, 186)
-    ctx.font = '18pt "HNB"'
-    ctx.fillText(timestring, 32, 242)
+    await drawCurrentEventTime(ctx, timestring, false)
 }
 
 async function drawOccupiedUnknown(ctx, end) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 106, 480, 169);
-    ctx.font = '42pt "HNB"'
-    ctx.fillStyle = "rgba(255, 255, 255, 1";
-    ctx.fillText("Booked", 32, 202)
-    ctx.font = '18pt "HNB"'
-    ctx.fillText("Until " + end, 32, 242)
+    await drawCurrentEventBlackBox(ctx)
+    await drawCurrentEventInfo(ctx, "Booked", false)
+    await drawCurrentEventTime(ctx, "Until " + end, false)
+}
+
+async function drawFreeUntil(ctx, end) {
+    await drawCurrentEventWhiteBox(ctx)
+    await drawCurrentEventInfo(ctx, "Available", true)
+    await drawCurrentEventTime(ctx, "Until " + end, true)
+}
+
+async function drawOpccupiedSoon(ctx, start) {
+    await drawCurrentEventWhiteBox(ctx)
+    await drawCurrentEventInfo(ctx, "Next Meeting", true)
+    await drawCurrentEventTime(ctx, "Starts soon - " + start, true)
 }
 
 async function drawHeader(ctx, room_name, room_number) {
@@ -35,8 +70,6 @@ async function drawHeader(ctx, room_name, room_number) {
 
     ctx.font = '15pt "HNB"'
     ctx.fillText(room_number, 248, 83)
-
-
 }
 
 
@@ -49,48 +82,14 @@ async function doImageThings() {
 
     await drawHeader(ctx, 'Meeting Room', '1000.0')
     // await drawOccupied(ctx, '[IDB] Status Meeting with HiWis', 'Liam Tirpitz', '9:00 - 10:00')
-    await drawOccupiedUnknown(ctx, "10:00")
-
-
-
-
-
+    // await drawOccupiedUnknown(ctx, "10:00")
+    // await drawFreeUntil(ctx, "10:00")
+    await drawOpccupiedSoon(ctx, '10:00')
 
     const out = fs.createWriteStream( 'out.png')
     const stream = canvas.createPNGStream()
     stream.pipe(out)
     out.on('finish', () =>  console.log('The PNG file was created.'))
-    // PImage.decodePNGFromStream(fs.createReadStream("template.png")).then(
-    //     (img) => {
-    //         // var fnt = PImage.registerFont(
-    //         //     "./fonts/HelveticaNeueLtCom-Bd.ttf",
-    //         //     "HN Bold",
-    //         // );
-    //         // fnt.loadSync();
-    //
-    //         var fnt2 = PImage.registerFont(
-    //             "./fonts/HelveticaNeueLtCom-lt.ttf",
-    //             "HN asd",
-    //         );
-    //         fnt2.loadSync();
-    //
-    //             console.log("size is", img.width, img.height);
-    //         var ctx = img.getContext("2d");
-    //         ctx.fillStyle = "rgba(0, 25, 234, 0.6";
-    //         ctx.font = "28pt 'HN Light'";
-    //         ctx.fillText("Meeting Room", 248, 62);
-    //         // ctx.font = "28pt 'HN Bold'";
-    //         // ctx.fillText("Meeting Room", 248, 62);
-    //
-    //         PImage.encodePNGToStream(img, fs.createWriteStream("out.png"))
-    //             .then(() => {
-    //                 console.log("wrote out the png file to out.png");
-    //             })
-    //             .catch((e) => {
-    //                 console.log("there was an error writing");
-    //             });
-    //     }
-    // )
 }
 
 doImageThings().then()
