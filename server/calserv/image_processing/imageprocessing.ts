247d69711b7da1e1e000167f1889e1c2f9b9a951
Liam Tirpitz <tirpitz@informatik.rwth-aachen.de>
2024-09-29 22:21:51 +0200

    Rewrite Image Processor Class

--- server/calserv/image_processing/imageprocessing.ts
+++ server/calserv/image_processing/imageprocessing.ts
@@ -3,119 +3,127 @@ import {SimpleEvent} from "../datamodels/SimpleEvent"
 import {registerFont, createCanvas, loadImage, CanvasRenderingContext2D, Canvas, Image} from 'canvas'
 import * as fs from 'fs';
 
-function drawCurrentEventTime(ctx: CanvasRenderingContext2D, timestring: string, black_font: boolean = false) {
-    ctx.fillStyle = "rgba(255, 255, 255, 1)";
-    if (black_font) {
-        ctx.fillStyle = "rgba(0, 0, 0, 1)";
+class ImageProcessor {
+    ctx: CanvasRenderingContext2D;
+    canvas: Canvas;
+    img: Promise<Image> = loadImage('image_processing/template.png')
+
+    constructor() {
+        this.canvas = this.setupCanvas();
+        this.ctx = this.canvas.getContext('2d')
     }
-    ctx.font = '18pt "HNB"'
-    ctx.fillText(timestring, 32, 242)
-}
 
-function drawCurrentEventBlackBox(ctx: CanvasRenderingContext2D) {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 106, 480, 169);
-}
-
-function drawCurrentEventWhiteBox(ctx: CanvasRenderingContext2D) {
-    ctx.lineWidth = 2;
-    ctx.beginPath()
-    ctx.strokeStyle = 'black';
-    ctx.strokeRect(-2, 106, 482, 169);
-    ctx.closePath()
-}
-
-function drawCurrentEventInfo(ctx: CanvasRenderingContext2D, info: string, black_font: boolean = false) {
-    ctx.font = '42pt "HNB"'
-    ctx.fillStyle = "rgba(255, 255, 255, 1)";
-    if (black_font) {
-        ctx.fillStyle = "rgba(0, 0, 0, 1)";
+    setupCanvas(): Canvas {
+        registerFont('image_processing/fonts/HelveticaNeueLTCom-Bd.ttf', { family: 'HNB' })
+        registerFont('image_processing/fonts/HelveticaNeueLTCom-Lt.ttf', { family: 'HNL' })
+        return createCanvas(480, 800)
     }
-    ctx.fillText(info, 32, 202)
-}
 
-function drawOccupied(ctx: CanvasRenderingContext2D, summary: string, organizer: string, timestring: string) {
-    drawCurrentEventBlackBox(ctx)
-    ctx.font = '18pt "HNB"'
-    ctx.fillStyle = "rgba(255, 255, 255, 1";
-    ctx.fillText(summary, 32, 160)
-    ctx.font = '18pt "HNL"'
-    ctx.fillText(organizer, 32, 186)
-    drawCurrentEventTime(ctx, timestring, false)
-}
-
-function drawOccupiedUnknown(ctx: CanvasRenderingContext2D, end: string) {
-    drawCurrentEventBlackBox(ctx)
-    drawCurrentEventInfo(ctx, "Booked", false)
-    drawCurrentEventTime(ctx, "Until " + end, false)
-}
-
-function drawFreeUntil(ctx: CanvasRenderingContext2D, end: string) {
-    drawCurrentEventWhiteBox(ctx)
-    drawCurrentEventInfo(ctx, "Available", true)
-    drawCurrentEventTime(ctx, "Until " + end, true)
-}
-
-function drawOpccupiedSoon(ctx: CanvasRenderingContext2D, start: string) {
-    drawCurrentEventWhiteBox(ctx)
-    drawCurrentEventInfo(ctx, "Next Meeting", true)
-    drawCurrentEventTime(ctx, "Starts soon - " + start, true)
-}
-
-function drawHeader(ctx: CanvasRenderingContext2D, img: Image, room_name: string, room_number: string) {
-    ctx.drawImage(img, 0, 0)
-
-    ctx.font = '21pt "HNB"'
-    ctx.fillText(room_name, 248, 62)
-
-    ctx.font = '15pt "HNB"'
-    ctx.fillText(room_number, 248, 83)
-}
-
-function setupCanvas(): Canvas {
-    registerFont('image_processing/fonts/HelveticaNeueLTCom-Bd.ttf', { family: 'HNB' })
-    registerFont('image_processing/fonts/HelveticaNeueLTCom-Lt.ttf', { family: 'HNL' })
-    return createCanvas(480, 800)
-}
-
-
-function getTimeStringFromDate(date: Date) {
-    return date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
-}
-
-function drawCurrentEvent(ctx:CanvasRenderingContext2D, event: SimpleEvent) {
-    const now: Date = new Date();
-    console.log(now, event.start)
-    if (event.start <= now && now <= event.end) {
-        if (event.summary) {
-            drawOccupied(ctx, event.summary, event.organizer, getTimeStringFromDate(event.start) + " - " + getTimeStringFromDate(event.end))
-        } else {
-            drawOccupiedUnknown(ctx, getTimeStringFromDate(event.end))
+    drawCurrentEventTime(timestring: string, black_font: boolean = false) {
+        this.ctx.fillStyle = "rgba(255, 255, 255, 1)";
+        if (black_font) {
+            this.ctx.fillStyle = "rgba(0, 0, 0, 1)";
         }
-    } else if (now < event.start && (event.start.valueOf() - now.valueOf()) < 15 * 60 * 1000) {
-        drawOpccupiedSoon(ctx, getTimeStringFromDate(event.start))
-    } else if (now < event.start) {
-        drawFreeUntil(ctx, getTimeStringFromDate(event.start))
-    } else {
-        console.log("No current event to draw?")
+        this.ctx.font = '18pt "HNB"'
+        this.ctx.fillText(timestring, 32, 242)
     }
 
+    drawCurrentEventBlackBox() {
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(0, 106, 480, 169);
+    }
+
+    drawCurrentEventWhiteBox() {
+        this.ctx.lineWidth = 2;
+        this.ctx.beginPath()
+        this.ctx.strokeStyle = 'black';
+        this.ctx.strokeRect(-2, 106, 482, 169);
+        this.ctx.closePath()
+    }
+
+    drawCurrentEventInfo(info: string, black_font: boolean = false) {
+        this.ctx.font = '42pt "HNB"'
+        this.ctx.fillStyle = "rgba(255, 255, 255, 1)";
+        if (black_font) {
+            this.ctx.fillStyle = "rgba(0, 0, 0, 1)";
+        }
+        this.ctx.fillText(info, 32, 202)
+    }
+
+    drawOccupied(summary: string, organizer: string, timestring: string) {
+        this.drawCurrentEventBlackBox()
+        this.ctx.font = '18pt "HNB"'
+        this.ctx.fillStyle = "rgba(255, 255, 255, 1";
+        this.ctx.fillText(summary, 32, 160)
+        this.ctx.font = '18pt "HNL"'
+        this.ctx.fillText(organizer, 32, 186)
+        this.drawCurrentEventTime(timestring, false)
+    }
+
+    drawOccupiedUnknown(end: string) {
+        this.drawCurrentEventBlackBox()
+        this.drawCurrentEventInfo("Booked", false)
+        this.drawCurrentEventTime("Until " + end, false)
+    }
+
+    drawFreeUntil(end: string) {
+        this.drawCurrentEventWhiteBox()
+        this.drawCurrentEventInfo("Available", true)
+        this.drawCurrentEventTime("Until " + end, true)
+    }
+
+    drawOpccupiedSoon(start: string) {
+        this.drawCurrentEventWhiteBox()
+        this.drawCurrentEventInfo("Next Meeting", true)
+        this.drawCurrentEventTime("Starts soon - " + start, true)
+    }
+
+    async drawHeader(room_name: string, room_number: string) {
+        this.ctx.drawImage(await this.img, 0, 0)
+
+        this.ctx.font = '21pt "HNB"'
+        this.ctx.fillText(room_name, 248, 62)
+
+        this.ctx.font = '15pt "HNB"'
+        this.ctx.fillText(room_number, 248, 83)
+    }
+
+    getTimeStringFromDate(date: Date) {
+        return date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+    }
+
+    drawCurrentEvent(event: SimpleEvent) {
+        const now: Date = new Date();
+        console.log(now, event.start)
+        if (event.start <= now && now <= event.end) {
+            if (event.summary) {
+                this.drawOccupied(event.summary, event.organizer, this.getTimeStringFromDate(event.start) + " - " + this.getTimeStringFromDate(event.end))
+            } else {
+                this.drawOccupiedUnknown(this.getTimeStringFromDate(event.end))
+            }
+        } else if (now < event.start && (event.start.valueOf() - now.valueOf()) < 15 * 60 * 1000) {
+            this.drawOpccupiedSoon(this.getTimeStringFromDate(event.start))
+        } else if (now < event.start) {
+            this.drawFreeUntil(this.getTimeStringFromDate(event.start))
+        } else {
+            console.log("No current event to draw?")
+        }
+    }
 }
+
 
 async function doImageThings() {
-    const canvas = setupCanvas()
-    const ctx:CanvasRenderingContext2D = canvas.getContext('2d')
-    const img: Image = await loadImage('image_processing/template.png')
-    await drawHeader(ctx, img, 'Meeting Room', '1000.0')
+    const imgProc = new ImageProcessor();
+    const header = imgProc.drawHeader('Meeting Room', '1000.0')
 
     const start = new Date(Date.parse("2024-09-29T13:12:00"));
     const end = new Date(Date.parse('2024-09-29T15:00:00'));
 
     const event = new SimpleEvent(start, end, "[IDB] Status Meeting with HiWis", "Liam Tirpitz")
-    await drawCurrentEvent(ctx, event)
-
+    imgProc.drawCurrentEvent(event)
+    await header
     const out = fs.createWriteStream( 'out.png')
-    const stream = canvas.createPNGStream()
+    const stream = imgProc.canvas.createPNGStream()
     stream.pipe(out)
     out.on('finish', () =>  console.log('The PNG file was created.'))
 }
