20f163423bf37b96b852aefe20cf6ec008f2e56a
Liam Tirpitz <tirpitz@informatik.rwth-aachen.de>
2024-09-30 14:16:28 +0200

    Work on Exchange Client #2

--- server/calserv/ews/ews.ts
+++ server/calserv/ews/ews.ts
@@ -1,20 +1,26 @@
-import {CalendarEvent, GetUserAvailabilityResults} from "ews-javascript-api";
+import {
+    AttendeeAvailability,
+    AttendeeInfo,
+    CalendarEvent,
+    ExchangeService,
+    GetUserAvailabilityResults,
+    TimeWindow
+} from "ews-javascript-api";
 import * as dotenv from 'dotenv'
+import * as ews from 'ews-javascript-api'
 dotenv.config()
+var config = require('../config/calendars.json');
 
-var ews = require('ews-javascript-api');
-//create ExchangeService object
-var exch = new ews.ExchangeService(ews.ExchangeVersion.Exchange2013);
+var exch: ExchangeService = new ews.ExchangeService(ews.ExchangeVersion.Exchange2010);
 exch.Credentials = new ews.WebCredentials(process.env.USER, process.env.PASS);
-//set ews endpoint url to use
-exch.Url = new ews.Uri("https://mail.rwth-aachen.de/EWS/Exchange.asmx"); // you can also use exch.AutodiscoverUrl
+exch.Url = new ews.Uri(config.exchange.endpoint);
 
-var attendee =[ new ews.AttendeeInfo("room-a@example.com")];
-//create timewindow object o request avaiability suggestions for next 48 hours, DateTime and TimeSpan object is created to mimic portion of .net datetime/timespan object using momentjs
-var timeWindow = new ews.TimeWindow(ews.DateTime.Now, ews.DateTime.Now.AddDays(2));
+var attendee: AttendeeInfo[] =[ new ews.AttendeeInfo("room-a@example.com")];
+var timeWindow: TimeWindow = new ews.TimeWindow(ews.DateTime.Now, ews.DateTime.Now.AddDays(2));
+
 exch.GetUserAvailability(attendee, timeWindow, ews.AvailabilityData.FreeBusyAndSuggestions)
     .then(function (availabilityResponse: GetUserAvailabilityResults) {
-        const responses = availabilityResponse.AttendeesAvailability.Responses.at(0)
+        const responses:AttendeeAvailability = availabilityResponse.AttendeesAvailability.Responses.at(0)
         for (let cEvent: CalendarEvent of responses.CalendarEvents) {
             console.log(cEvent)
         }
