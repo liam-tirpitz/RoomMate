f9a37e722e390fac4c00aa125ead5fc47483c454
Laurenz Neumann <laurenz.neumann@dbis.rwth-aachen.de>
2024-10-02 12:20:49 +0200

    first footer setup, moved header files

--- client/room-occupancy/Room-Occupancy-Client/src/Screen.cpp
+++ client/room-occupancy/Room-Occupancy-Client/src/Screen.cpp
@@ -2,39 +2,31 @@
 
 UBYTE *BlackImage;
 
+UWORD Imagesize = ((SCREEN_WIDTH % 8 == 0) ? (SCREEN_WIDTH / 8 ) : (SCREEN_WIDTH / 8 + 1)) * SCREEN_HEIGHT;
+
+Screen::Screen(FooterState* footer_state) : footer_state(footer_state) {};
+
 void Screen::setup() {
     DEV_Module_Init();
     EPD_7IN5_V2_Init();
-    EPD_7IN5_V2_Clear();
     DEV_Delay_ms(500);
-
-    UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0) ? (EPD_7IN5_V2_WIDTH / 8 ) : (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
     if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
         printf("Failed to apply for black memory...\r\n");
         while (1);
     }
-    printf("Paint_NewImage\r\n");
-    Paint_NewImage(BlackImage, EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT, 0, WHITE);
-    Paint_SetRotate(90);
-
-    // EPD_7IN5_V2_Init_Fast();  
-    // printf("SelectImage:BlackImage\r\n");
+    Paint_NewImage(BlackImage, SCREEN_WIDTH, SCREEN_HEIGHT, ROTATE_270, WHITE);
     Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
+    Paint_SetRotate(270);
 
 }
 
+void Screen::draw() {
+    Paint_Clear(WHITE);
+    draw_footer();
+    EPD_7IN5_V2_Display(BlackImage);
+}
 
-void Screen::printThings() {
-    Paint_DrawBitMap(example);
-
-
-//   Paint_DrawString_EN(10, 0, "Test", &Font24, WHITE, BLACK);
-// //   Paint_DrawString_EN(10, 30, ",", &Font24, WHITE, BLACK);
-
-
-  printf("EPD_Display\r\n");
-  EPD_7IN5_V2_Display(BlackImage);
-  DEV_Delay_ms(2000);
-//   EPD_7IN5_V2_Sleep();
+void Screen::draw_footer() {
+    Paint_DrawRectangle(FOOTER_X_START, FOOTER_Y_START, SCREEN_WIDTH-2, SCREEN_HEIGHT-2, BLACK, DOT_PIXEL_2X2, DRAW_FILL_EMPTY);
+    Paint_DrawString_EN(FOOTER_X(70), FOOTER_Y(20), "Hallo Welt!", &Font16, WHITE, BLACK);
 }
