ab18e981470dc3db7d7e2ca4beb59a39fcb5044c
Liam Tirpitz <tirpitz@informatik.rwth-aachen.de>
2024-11-17 14:43:20 +0100

    dev(server): Fix build

--- server/datamodels/IRoom.ts
+++ server/datamodels/IRoom.ts
@@ -2,8 +2,6 @@ import {IEWSCalendarInfo} from "./IEWSCalendarInfo";
 import {IICalCalendarInfo} from "./IICalCalendarInfo";
 import {IPerson} from "./IPerson";
 import {IDevice} from "./IDevice";
-import {ObjectId} from "mongodb";
-
 export interface IRoom {
     room_number: number
     id_string: string
